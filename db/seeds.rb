88a201a179b6bdf4736d64cab353fc302c6e5e37
Alex Diwa <atdiwa@gmail.com>
2019-04-30 14:25:41 +1000

    seeding database with users and locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+locations = [
+  "CBD",
+  "Inner West",
+  "North Shore",
+  "Western Suburbs",
+  "Eastern Suburbs"
+]
+
+locations.each do |location|
+  Location.create(name: location)
+  puts "Created location: #{location}"
+end
+
+for i in 1..10
+  User.create(
+    email: "ama+#{i}@test.com",
+    password: "testpass",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    role: rand(0..1),
+    age: rand(18..40),
+    bio: Faker::Hipster.sentence(3, true, 4),
+    portfolio_url: "http://#{Faker::Internet.domain_name}",
+    profile_complete: false,
+    location_id: rand(1..5),
+    company: Faker::Company.name,
+    occupation: Faker::Name.last_name
+  )
+  puts "Created #{i} users"
+end
+
