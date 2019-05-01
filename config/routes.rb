ae740959069dcde14ffa8ad1c94188de2df1faa9
Michael Bahari <bahayana@gmail.com>
2019-05-01 16:13:56 +1000

    added pages controller and donate button

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: "users#index"
   resources :users
+  get "/pages/:page" => "pages#show"
 end
