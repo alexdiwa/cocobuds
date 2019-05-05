fa8da4a222b41c991d95d62d5635851650e761ee
Alex <atdiwa@gmail.com>
2019-05-05 16:26:43 +1000

    added redirect to prevent users from messaging themselves

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,7 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-
-  before_action do
-    @conversation = Conversation.find(params[:conversation_id])
-  end
+  before_action :set_conversation, only: [:index, :create]
+  before_action :same_user_check, only: [:index]
 
   def index
     @messages = @conversation.messages.order(:created_at)
@@ -23,7 +21,17 @@ class MessagesController < ApplicationController
   end
 
   private
+    def set_conversation
+      @conversation = Conversation.find(params[:conversation_id])
+    end
+
     def message_params
       params.require(:message).permit(:body, :user_id)
+    end
+
+    def same_user_check
+      if @conversation.receiver_id == @conversation.sender_id
+        redirect_to user_path(current_user.id), notice: 'You can\'t send a message to yourself!'
+      end
     end
 end
